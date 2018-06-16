0348ff1a1b408da0452f19be1a5251e1d23c391d
Katie McLaughlin <katie@glasnt.com>
2018-06-16 19:49:26 +1000

    black™️

--- lyrebird-cli.py
+++ lyrebird-cli.py
@@ -1,6 +1,7 @@
 import os
 import tweepy
 from colorama import Fore, Back, Style, init
+
 init(autoreset=True)
 
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
@@ -16,31 +17,36 @@ api = tweepy.API(auth)
 
 
 def sendTweet(tweet, hashtags, response_id):
-    combined_status = tweet + ' ' + hashtags
+    combined_status = tweet + " " + hashtags
     if response_id is None:
         status_response = api.update_status(status=combined_status)
     else:
-        status_response = api.update_status(status=combined_status,
-                                            in_reply_to_status_id=response_id)
+        status_response = api.update_status(
+            status=combined_status, in_reply_to_status_id=response_id
+        )
     return {
-        'status_id': status_response.id,
-        'status_text': status_response.text,
-        'responded_to': status_response.in_reply_to_status_id
+        "status_id": status_response.id,
+        "status_text": status_response.text,
+        "responded_to": status_response.in_reply_to_status_id,
     }
 
 
 print("Welcome to " + Back.GREEN + Fore.BLACK + " Lyrebird! ")
 
-hashtags = input(Back.BLUE + Fore.BLACK +
-                 " What hashtags should be appended to each tweet? " +
-                 Style.RESET_ALL + " ")
+hashtags = input(
+    Back.BLUE
+    + Fore.BLACK
+    + " What hashtags should be appended to each tweet? "
+    + Style.RESET_ALL
+    + " "
+)
 
 while True:
     tweet = input("What do you want to tweet? ")
     response = sendTweet(tweet, hashtags, response_id)
-    response_id = response['status_id']
-    print("\nReply to: " + Back.BLUE + Fore.BLACK + " " +
-          str(response['status_id']) + " ")
-    print("ID: " + Back.YELLOW + Fore.BLACK + " " +
-          str(response['status_id']) + " ")
-    print("Tweet: " + str(response['status_text']) + "\n")
+    response_id = response["status_id"]
+    print(
+        "\nReply to: " + Back.BLUE + Fore.BLACK + " " + str(response["status_id"]) + " "
+    )
+    print("ID: " + Back.YELLOW + Fore.BLACK + " " + str(response["status_id"]) + " ")
+    print("Tweet: " + str(response["status_text"]) + "\n")
