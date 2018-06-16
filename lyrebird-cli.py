f303882744e42078dfb6b0631ffe70de5c833dab
Katie McLaughlin <katie@glasnt.com>
2018-06-16 20:22:46 +1000

    Save and offer to try again if tweet too long

--- lyrebird-cli.py
+++ lyrebird-cli.py
@@ -4,25 +4,35 @@ from colorama import Fore, Back, Style, init
 
 init(autoreset=True)
 
+TWEET_LENGTH_LIMIT = 280
+
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
-response_id = None
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+response = {"status_id": None, "status_text": None, "responded_to": None}
 
-def sendTweet(tweet, hashtags, response_id):
+
+def sendTweet(tweet, hashtags, response):
     combined_status = tweet + " " + hashtags
+
+    if len(combined_status) >= TWEET_LENGTH_LIMIT:
+        print(
+            Fore.RED + "Error! Tweet too long! " + str(len(combined_status))
+        ) + " characters"
+        return response
+
     if response_id is None:
         status_response = api.update_status(status=combined_status)
     else:
         status_response = api.update_status(
-            status=combined_status, in_reply_to_status_id=response_id
+            status=combined_status, in_reply_to_status_id=response["id"]
         )
     return {
         "status_id": status_response.id,
@@ -52,7 +62,7 @@ hashtags = input(
 
 while True:
     tweet = input("What do you want to tweet? ")
-    response = sendTweet(tweet, hashtags, response_id)
+    response = sendTweet(tweet, hashtags, response)
     response_id = response["status_id"]
     print(
         "\nReply to: " + Back.BLUE + Fore.BLACK + " " + str(response["status_id"]) + " "
